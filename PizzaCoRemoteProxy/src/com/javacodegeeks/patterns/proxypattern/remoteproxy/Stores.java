88233c7f83b8d89e34f639c1c23499f22490e591
Emilio Wong <emilews.ew@gmail.com>
2019-09-22 23:25:27 -0700

    P2 no longer exists

--- PizzaCoRemoteProxy/src/com/javacodegeeks/patterns/proxypattern/remoteproxy/Stores.java
+++ PizzaCoRemoteProxy/src/com/javacodegeeks/patterns/proxypattern/remoteproxy/Stores.java
@@ -1,7 +1,10 @@
 package com.javacodegeeks.patterns.proxypattern.remoteproxy;
 
+import com.javacodegeeks.patterns.proxypattern.remoteproxy.database.DatabaseActions;
+
 import java.io.*;
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Stores {
     //Now we use a database omg
@@ -17,116 +20,32 @@ public class Stores {
     }
 
 
-    public String getStore(String name) {
+    public String getStore(DatabaseActions database, String name) throws SQLException {
         StringBuilder sb = new StringBuilder();
-        for (Store t : data) {
-            if (t.getName().equals(name)) {
-                sb.append(t.getName());
-                sb.append(",");
-                sb.append(t.getAddress());
-                sb.append(",");
-                sb.append(t.getNumber());
-                break;
-            }
-        }
+        ResultSet resultSet = database.Read("SELECT * FROM store WHERE storename = " + "'" + name +"';");
+        resultSet.next();
+        sb.append(resultSet.getString())
         return sb.toString();
     }
 
     public boolean addNewStore(String name, String address, int id, int sales) throws IOException {
-        for(Store s: data){
-            if(s.getNumber() == id){
-                return false;
-            }
-        }
-        Store store = new Store(name, address, id, sales);
-        data.add(store);
-        StringBuilder sb = new StringBuilder();
-        bufferedWriter = new BufferedWriter(new FileWriter(new File(CSV_FILE_PATH)));
-        for(Store s : data){
-            try {
-                sb.append(s.getName());
-                sb.append(",");
-                sb.append(s.getAddress());
-                sb.append(",");
-                sb.append(s.getNumber());
-                sb.append(",");
-                sb.append(s.getDailySales());
-                bufferedWriter.write(sb.toString());
-                bufferedWriter.write("\n");
-                sb.setLength(0);
-            }catch (Exception e){
-                return false;
-            }
-        }
-        bufferedWriter.flush();
-        bufferedWriter.close();
+
         return true;
     }
 
     public String getDailySalesByStore(String name) {
         StringBuilder sb = new StringBuilder();
-        for (Store t : data) {
-            if (t.getName().equals(name)) {
-                sb.append(t.getName());
-                sb.append(",");
-                sb.append(t.getNumber());
-                sb.append(",");
-                sb.append(t.getDailySales());
-                break;
-            }
-        }
+
         return sb.toString();
     }
 
     public int getOverallSales(){
-        int sales = 0;
-        for(Store s : data){
-            sales += s.getDailySales();
-        }
-        return sales;
+
+        return 0;
     }
 
     public String getAllStoreNames(){
         StringBuilder sb = new StringBuilder();
-        int size = data.size();
-        int i = 0;
-        for(Store s : data){
-            if(i != size){
-                sb.append(s.getName());
-                sb.append(",");
-            }else {
-                sb.append(s.getName());
-            }
-            i++;
-        }
         return sb.toString();
-    }
-
-
-    private class Store {
-        private String name;
-        private String address;
-        private int code;
-        private int dailySales;
-        public Store(String name, String address, int code, int dailySales) {
-            this.name = name;
-            this.address = address;
-            this.code = code;
-            this.dailySales = dailySales;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getAddress() {
-            return address;
-        }
-
-        public int getNumber() {
-            return code;
-        }
-
-        public int getDailySales(){ return dailySales; }
     }
 }
